4cb98d5638782d95fdf22d8e2232da5511f853dd
Jordan Place <tapman90@gmail.com>
2020-05-27 14:51:49 -0700

    more real links

--- src/site/_includes/components/modal.js
+++ src/site/_includes/components/modal.js
@@ -3,12 +3,14 @@ $("[data-modal-declare]").each(function () {
   const thisModalName = thisModal.data("modal-declare");
   const thisModalRef = thisModal.get();
 
-  thisModal.find("[data-modal-close]").click(function () {
+  thisModal.find("[data-modal-close]").click(function (event) {
+    event.preventDefault();
     thisModal.addClass("modal-closed");
     bodyScrollLock.enableBodyScroll(thisModalRef);
   });
 
-  $(`[data-modal-trigger='${thisModalName}']`).click(function () {
+  $(`[data-modal-trigger='${thisModalName}']`).click(function (event) {
+    event.preventDefault();
     thisModal.removeClass("modal-closed");
     bodyScrollLock.disableBodyScroll(thisModalRef);
   });
--- src/site/_includes/components/nav.js
+++ src/site/_includes/components/nav.js
@@ -1,11 +1,13 @@
 $("[data-nav-declare]").each(function () {
   const thisNav = $(this);
 
-  thisNav.find("[data-nav-close]").click(function () {
+  thisNav.find("[data-nav-close]").click(function (event) {
+    event.preventDefault();
     thisNav.addClass("nav_closed");
   });
 
-  $(`[data-nav-trigger]`).click(function () {
+  $(`[data-nav-trigger]`).click(function (event) {
+    event.preventDefault();
     thisNav.removeClass("nav_closed");
   });
 
