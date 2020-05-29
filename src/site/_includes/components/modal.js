cc6b81d302299862cf8f10f8c1f14ab1e53d451d
Jordan Place <tapman90@gmail.com>
2020-05-28 17:24:09 -0700

    reorganize footer and work on mobile modal issues

--- src/site/_includes/components/modal.js
+++ src/site/_includes/components/modal.js
@@ -1,18 +1,18 @@
 $("[data-modal-declare]").each(function () {
   const thisModal = $(this);
   const thisModalName = thisModal.data("modal-declare");
-  const thisModalRef = thisModal.get();
+  const thisModalDialogRef = thisModal.find("[data-modal-dialog]")[0];
 
   thisModal.find("[data-modal-close]").click(function (event) {
     event.preventDefault();
     thisModal.addClass("modal-closed");
-    bodyScrollLock.enableBodyScroll(thisModalRef);
+    bodyScrollLock.enableBodyScroll(thisModalDialogRef);
   });
 
   $(`[data-modal-trigger='${thisModalName}']`).click(function (event) {
     event.preventDefault();
     thisModal.removeClass("modal-closed");
-    bodyScrollLock.disableBodyScroll(thisModalRef);
+    bodyScrollLock.disableBodyScroll(thisModalDialogRef);
   });
 
   thisModal.addClass("modal-closed");
