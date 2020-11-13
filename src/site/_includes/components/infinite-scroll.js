27c68e6fcd9adb611f01482a613394590e11bb19
Jordan Place <tapman90@gmail.com>
2020-11-12 17:45:06 -0800

    rudimentary infinite scroll

---
+++ src/site/_includes/components/infinite-scroll.js
@@ -0,0 +1,29 @@
+const InfiniteScroll = function (onScrollToBottom) {
+  this.isBusy = false;
+  this.onScrollToBottom = onScrollToBottom;
+};
+
+InfiniteScroll.prototype.init = function () {
+  window.addEventListener("scroll", () => {
+    const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+    if (scrollTop + clientHeight > scrollHeight - 5) {
+      this.hitBottom();
+    }
+  });
+};
+
+InfiniteScroll.prototype.hitBottom = function () {
+  console.log("yo");
+  if (this.isBusy) {
+    return;
+  }
+  this.isBusy = true;
+  this.onScrollToBottom().then(
+    () => {
+      this.isBusy = false;
+    },
+    () => {
+      this.isBusy = false;
+    }
+  );
+};
