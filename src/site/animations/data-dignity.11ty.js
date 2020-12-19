54f7fdae4fbb7be976a1c992ea67d5e7727a1ac3
Jordan Place <tapman90@gmail.com>
2020-12-19 10:56:43 -0800

    works

--- src/site/animations/data-dignity.11ty.js
+++ src/site/animations/data-dignity.11ty.js
@@ -15,9 +15,10 @@ class Thing {
     <div style="width: 512px" class="border mx-auto bg-golden-fizz">
     <svg id="data-dignity" width="100%" viewBox="0 0 100 128" xmlns="http://www.w3.org/2000/svg">
     <g id="data-dignity-rotate">
-    ${[0, 1, 2, 3, 4, 5, 6, 7]
+    ${[6, 5, 4, 3, 2, 1, 0]
       .map(
-        (_, index) => `<circle
+        (index) => `<circle
+  id="circle-${index}"
   cx="${Math.sin(index * radianInterval) * scale + 50}"
   cy="${Math.cos(index * radianInterval) * scale * -1 + 64}"
   r="${radius}"
@@ -28,6 +29,31 @@ class Thing {
       )
       .join("\n")}
     </g>
+    <style>
+    #data-dignity #data-dignity-rotate {
+      transform-origin: center center;
+      transform: rotateZ(0);
+      transition: transform 1s;
+    }
+    
+    #data-dignity:hover #data-dignity-rotate {
+      transform: rotateZ(51.43deg);
+    }
+    ${[6, 5, 4, 3, 2, 1, 0]
+      .map(
+        (index) => `
+#data-dignity #circle-${index} {
+  transform: translate(0, 0);
+  transition: transform 1s;
+}
+#data-dignity:hover #circle-${index} {
+  transform: translate(${
+    Math.sin(index * radianInterval) * -1 * (scale * 0.7)
+  }%, ${Math.cos(index * radianInterval) * (scale * 0.7) * (100 / 128)}%);
+}`
+      )
+      .join("\n")}
+    </style>
     </svg>
   </div>
     `;
