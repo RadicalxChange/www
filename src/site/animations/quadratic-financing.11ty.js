35bb28b93c16e7904c57df1c7a258e166bf2461d
Jordan Place <tapman90@gmail.com>
2020-12-20 13:06:38 -0800

    qf

---
+++ src/site/animations/quadratic-financing.11ty.js
@@ -0,0 +1,79 @@
+class Thing {
+  data() {
+    return {
+      layout: "layouts/_base.njk",
+    };
+  }
+
+  render() {
+    const size = 10;
+    const innerPadding = 2;
+    const timeStep = 500;
+
+    const initialFills = [0, 4, 8, 9, 12, 13, 14, 15];
+    const squares = [];
+    let index = 0;
+    let yOffset = 41;
+    for (let r = 0; r < 4; r++) {
+      let xOffset = 27;
+      for (let c = 0; c < 4; c++) {
+        squares.push(
+          `<rect id="rect-${index}" x="${xOffset}" y="${yOffset}" width="${size}" height="${size}" fill="${
+            initialFills.includes(index) ? "#FAFFC3" : "transparent"
+          }" stroke="black" vector-effect="non-scaling-stroke"/>`
+        );
+        index++;
+        xOffset += size + innerPadding;
+      }
+      yOffset += size + innerPadding;
+    }
+
+    return `
+    <div style="width: 512px" class="border mx-auto bg-golden-fizz">
+    <svg id="quadratic-financing" width="100%" viewBox="0 0 100 128" xmlns="http://www.w3.org/2000/svg">
+      ${squares.join("\n")}
+      <style>
+      #rect-1,
+      #rect-2,
+      #rect-3 {
+        transition: fill ${timeStep}ms;
+      }
+
+      #rect-5,
+      #rect-6,
+      #rect-7 {
+        transition: fill ${timeStep * 2}ms;
+      }
+
+      #rect-10,
+      #rect-11 {
+        transition: fill ${timeStep * 3}ms;
+      }
+
+      #quadratic-financing:hover #rect-1,
+      #quadratic-financing:hover #rect-2,
+      #quadratic-financing:hover #rect-3 {
+        fill: #FAFFC3;
+        transition: fill ${timeStep * 3}ms;
+      }
+
+      #quadratic-financing:hover #rect-5,
+      #quadratic-financing:hover #rect-6,
+      #quadratic-financing:hover #rect-7 {
+        fill: #FAFFC3;
+        transition: fill ${timeStep * 2}ms;
+      }
+
+      #quadratic-financing:hover #rect-10,
+      #quadratic-financing:hover #rect-11 {
+        fill: #FAFFC3;
+        transition: fill ${timeStep}ms;
+      }
+      </style>
+    </svg>
+  </div>
+    `;
+  }
+}
+
+module.exports = Thing;
