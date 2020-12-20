feafd9042dfa1babbc2344ebfeed16d0efeb96d2
Jordan Place <tapman90@gmail.com>
2020-12-20 15:38:16 -0800

    redo home page

--- src/site/animations/quadratic-voting.11ty.js
+++ src/site/animations/quadratic-voting.11ty.js
@@ -1,69 +1,61 @@
+const { makeWebComponent } = require("./make-wc");
+
 class Thing {
   data() {
     return {
-      layout: "layouts/_base.njk",
+      permalink: "animations/quadratic-voting.js",
     };
   }
 
   render() {
-    return `
-    <div id="thing" style="width: 512px" class="border mx-auto bg-golden-fizz">
-    <svg-container>
-    <div id="canvas">
-      <div id="canvas__inner">
-        <div id="quadratic-voting"></div>
-      </div>
-    </div>
-    <style>
-    #canvas {
-      position: relative;
-      padding-top: 128%;
-    }
+    return makeWebComponent(
+      "QuadraticVotingAnimation",
+      "quadratic-voting-animation",
+      `
+<style>
+  #canvas {
+    position: relative;
+    padding-top: 128%;
+  }
 
-    #canvas__inner {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-    }
+  #canvas__inner {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 
-    /* https://mycolor.space/gradient?ori=to+bottom&hex=%23EDFF38&hex2=%23FAFFC3&sub=1 */
-    #quadratic-voting {
-      width: 100%;
-      height: 100%;
-      border: 1px solid black;
-      border-radius: 50%;
-      background-image: linear-gradient(to bottom, #edff38, #f1ff64, #f4ff86, #f7ffa5, #faffc3, #f7ffa5, #f4ff86, #f1ff64, #edff38);
-      background-size: auto 300%;
-    }
+  /* https://mycolor.space/gradient?ori=to+bottom&hex=%23EDFF38&hex2=%23FAFFC3&sub=1 */
+  #quadratic-voting {
+    width: 100%;
+    height: 100%;
+    border: 1px solid black;
+    border-radius: 50%;
+    background-image: linear-gradient(to bottom, #edff38, #f1ff64, #f4ff86, #f7ffa5, #faffc3, #f7ffa5, #f4ff86, #f1ff64, #edff38);
+    background-size: auto 300%;
+  }
 
-    #quadratic-voting:hover {
-      animation: linear infinite gradient-rotation 2.5s running;
+  #quadratic-voting:hover {
+    animation: linear infinite gradient-rotation 2.5s running;
+  }
+
+  @keyframes gradient-rotation {
+    0% {
+      background-position-y: 0%;
     }
-    
-    @keyframes gradient-rotation {
-      0% {
-        background-position-y: 0%;
-      }
-      100% {
-        background-position-y: 150%;
-      }
-    }
-    </style>
-    </svg-container>
-  </div>
-  <script>
-  class SVGContainer extends HTMLElement {
-    constructor() {
-      super();
-      const shadowRoot = this.attachShadow({mode: 'closed'});
-      shadowRoot.append(...this.childNodes);
+    100% {
+      background-position-y: 150%;
     }
   }
-  customElements.define('svg-container', SVGContainer);
-  </script>
-    `;
+</style>
+<div id="canvas">
+  <div id="canvas__inner">
+    <div id="quadratic-voting"></div>
+  </div>
+</div>
+`
+    );
   }
 }
 
