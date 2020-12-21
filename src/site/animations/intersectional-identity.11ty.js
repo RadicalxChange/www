49da7f72c037adb34a4b57fedee07462d7c5992a
Jordan Place <tapman90@gmail.com>
2020-12-21 15:24:11 -0800

    better

--- src/site/animations/intersectional-identity.11ty.js
+++ src/site/animations/intersectional-identity.11ty.js
@@ -13,30 +13,30 @@ class Thing {
       "intersectional-identity-animation",
       `
   <div style="display: flex; align-items: center; padding: 0 7%">
-    <svg id="intersectional-identity" width="100%" viewBox="0 0 80 100" xmlns="http://www.w3.org/2000/svg">
-      <path id="shape-1" d="M 10,19 h 20 v 50 h 20 v 20 h -40 v -70" fill="#FAFFC3" stroke="black" vector-effect="non-scaling-stroke"/>
-      <path id="shape-2" d="M 30,19 h 40 v 25 h -40 v -25" fill="#FAFFC3" stroke="black" vector-effect="non-scaling-stroke"/>
-      <path id="shape-3" d="M 30,44 h 40 v 45 h -20 v -20 h -20 v -25" fill="#FAFFC3" stroke="black" vector-effect="non-scaling-stroke"/>
+    <svg id="intersectional-identity" width="100%" viewBox="0 0 80 90" xmlns="http://www.w3.org/2000/svg">
+      <path id="shape-1" d="M 10,9 h 20 v 50 h 20 v 20 h -40 v -70" fill="#FAFFC3" stroke="black" vector-effect="non-scaling-stroke"/>
+      <path id="shape-2" d="M 30,9 h 40 v 25 h -40 v -25" fill="#FAFFC3" stroke="black" vector-effect="non-scaling-stroke"/>
+      <path id="shape-3" d="M 30,34 h 40 v 45 h -20 v -20 h -20 v -25" fill="#FAFFC3" stroke="black" vector-effect="non-scaling-stroke"/>
       <style>
       #shape-1 {
-        transform: translate(-10%, 10%) rotate(-2deg);
+        transform: translate(-10%, 10%);
         transition: transform 1s;
       }
 
       #shape-2 {
-        transform: translate(5%, -15%) rotate(4deg) ;
+        transform: translate(5%, -9%);
         transition: transform 1s;
       }
 
       #shape-3 {
-        transform: translate(10%, 5%) rotate(1deg);
+        transform: translate(10%, 5%);
         transition: transform 1s;
       }
 
       #intersectional-identity:hover #shape-1,
       #intersectional-identity:hover #shape-2,
       #intersectional-identity:hover #shape-3 {
-        transform: translate(0, 0) rotate(0);
+        transform: translate(0, 0);
       }
       </style>
     </svg>
