fc4ba24c70d1fa68c812b8348da4b7c5efb60bee
alexrandaccio <acrandaccio@gmail.com>
2021-01-11 16:00:42 -0500

    xpost conference videos to kiosk

--- src/site/kiosk/videos/_video-pages.11tydata.js
+++ src/site/kiosk/videos/_video-pages.11tydata.js
@@ -1,6 +1,9 @@
 const v2020Panels = require("../../../data/videos/2020-panels");
+const v2020Conference = require("../../../data/videos/2020-conference.js");
+const v2019Conference = require("../../../data/videos/2019-conference.js");
+const v2019Berlin = require("../../../data/videos/2019-berlin.js");
 
-const videos = [
+var videos = [
   ...v2020Panels.map((x) => ({
     date: x.date,
     title: x.title,
@@ -10,5 +13,38 @@ const videos = [
     description: x.description,
   })),
 ];
+
+videos = videos.concat([
+  ...v2020Conference.map((x) => ({
+    date: x.date,
+    title: x.title,
+    postHeader: x.title,
+    postAuthor: x.speakers,
+    videoId: x.videoId,
+    description: x.description,
+  })),
+]);
+
+videos = videos.concat([
+  ...v2019Conference.map((x) => ({
+    date: x.date,
+    title: x.title,
+    postHeader: x.title,
+    postAuthor: x.speakers,
+    videoId: x.videoId,
+    description: x.description,
+  })),
+]);
+
+videos = videos.concat([
+  ...v2019Berlin.map((x) => ({
+    date: x.date,
+    title: x.title,
+    postHeader: x.title,
+    postAuthor: x.speakers,
+    videoId: x.videoId,
+    description: x.description,
+  })),
+]);
 
 module.exports = { videos };
