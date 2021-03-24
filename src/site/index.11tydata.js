87172229c4a2ae006c521c2f1592baa137b56d72
alexrandaccio <acrandaccio@gmail.com>
2021-03-24 12:45:58 -0400

    before tv

--- src/site/index.11tydata.js
+++ src/site/index.11tydata.js
@@ -1,22 +1,19 @@
 module.exports = {
   rxcTv: {
     isOn: false,
-    title: "Data Coalitions in Development: Let's Unite!",
-    description: `February 25th, 3:00 pm to 4:30 pm UTC
+    title: "RadicalxChange Voice: An Introduction",
+    description: `March 25th, 5:00 pm to 6:30 pm UTC
 
-Many people in the RadicalxChange community and data science arena agree that we need collective bargaining power to make digital networks genuinely democratic and transparent. In this spirit, the speakers of this panel are on the mission of uniting the world's data producers. They are true leaders in this space and share their experiences from their everyday work for data dignity.
+RadicalxChange Voice is a new governance tool and experiment to bring in the RxC community to guide RxC Foundation's work. Hopefully, if the experiment turns out well, we can help other communities adopt it to enable a broader range of democratic participation. It also features some new thinking about QF, Pol.is, and QV. As we get ready to try it out, the Foundation's team introduces you to RxC Voice's end-to-end democratic process and explains the thinking behind it. Most importantly, it is your opportunity to ask us anything about it.
 
 
 ## Speakers
 
-Enoch Liang is a California-based lawyer and entrepreneur, first founding a law firm (LTL Attorneys) and then helping start an artificial intelligence software company (LegalMation).  Enoch helped start The Data Dividend Project to help consumers collectively exercise their data rights and to collectively bargain with tech companies.
+Jennifer Lyn Morone is RadicalxChange Foundation’s CEO and a multidisciplinary visual artist, activist, and filmmaker. Her work focuses on the human experience with technology, economics, politics, and identity, and the moral and ethical issues that arise from such systems. Her interests lie in exploring ways of creating social justice and equal distribution of the future. Morone is a trained sculptor with BFA from SUNY Purchase and earned her MA in Design Interactions at the Royal College of Art in London with Dunne and Raby. Her work has been presented at institutions, festivals, museums, and galleries around the world, including ZKM, Kunsthalle Düsseldorf, Ars Electronica, HEK, the Martin Gropius Bau, the Science Gallery, Transmediale, SMBA, Carroll/Fletcher Gallery, panke.gallery, Aksioma, Drugo more, and featured extensively on international media outlets such as The Economist, WIRED, WMMNA, Vice, the Guardian, BBC World News, Tagesspiegel, Netzpolitik, the Observer.
 
-Tracey Bowen is the Founder of CONTROLR - an inclusive, decentralized Data Union - a Data Researcher at the South West Creative Technology Network, Mozilla Creative Media Award Recipient, and Post Grad Law student – oh yeah, and Mercury Music Prize Winner (a very, very long time ago)!
+Matt Prewitt is RadicalxChange Foundation’s president, a writer and blockchain industry advisor, and a former plaintiff’s side antitrust and consumer class action litigator and federal law clerk.
 
-
-## Moderator
-
-Crystal Good (she/her/hers) is hard to put in just one box. She prefers Affrilachian artist, digital media entrepreneur, and Social Media Senator For The Digital District Of West Virginia, a completely made-up but totally real Office. She is a West Virginia University Newstart Fellow and the author of “Valley Girl.” Follow her on Twitter, @cgoodwoman.`,
-    slidoUrl: "https://app.sli.do/event/qppszbbm",
+Alex Randaccio is a Project Developer for RadicalxChange. He leverages an interdisciplinary background in software development, design, research, and writing to build out and advance RadicalxChange projects. He studied computer science and political science at Brown University. Alex proudly hails from Buffalo, New York`,
+    slidoUrl: "https://app.sli.do/event/1dgthink",
   },
 };
