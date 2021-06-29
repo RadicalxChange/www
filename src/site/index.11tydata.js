19f965b7e2f3519b3402cbcae2d25c0f66950de8
alexrandaccio <acrandaccio@gmail.com>
2021-06-29 16:30:25 -0400

    before tv

--- src/site/index.11tydata.js
+++ src/site/index.11tydata.js
@@ -1,22 +1,16 @@
 module.exports = {
   rxcTv: {
     isOn: false,
-    title: "Quadratic Voting at Work",
-    description: `May 11th, 4:00 pm to 5:30 pm UTC
+    title: "RxC Voice: From Election Insights to Action",
+    description: `June 30th, 6:00 pm to 7:00 pm UTC
 
-Quadratic Voting offers hope to revitalize collective decision-making in a wide range of domains in society and the economy, e.g., corporations, governments, unions, games, ratings, research, et cetera. An increasing number of examples support that hope in this radical voting method. In this panel discussion, you hear from current practices by policy-makers in the Colorado government and academic researchers and their insights from working with Quadratic Voting in preference polling.
+RxC Voice is RadicalxChange Foundation's new governance tool combining liquid democracy, online deliberation, and quadratic voting using QF (Quadratic Funding), Pol.is, and QV (Quadratic Voting).
 
-## Speakers
+Our first pilot asked our delegation, "What activities should the RadicalxChange Foundation prioritize in 2021-2022?" The results can be seen here and we look forward to sharing reflections on the process including a Q&A with the community.
 
-Charlotte Cavaille is an Assistant Professor at the Ford School of Public Policy at the University of Michigan. Before moving to Michigan, she was an Assistant Professor at the School of Foreign Service at Georgetown University. She was also a fellow at the Center for the Study of Democratic Politics at Princeton University. She received a Ph.D. in Government and Social Policy from Harvard University in November 2014. Some of her work appeared in The Journal of Politics and the American Political Science Review. Her research examines the dynamics of popular attitudes towards redistributive social policies at a time of rising inequality, high fiscal stress, and high levels of immigration.
+We'll go over the curation of the ballot, share interesting insights, look at the next steps for the project, and examine the mechanics of how the vote came to be.
 
-Senator Chris Hansen represents Senate District 31 in the Colorado State Senate. He specializes in energy sector economics and data analytics, with 20+ years of experience in the global energy industry and five years in the Colorado General Assembly. He currently serves on the Joint Budget Committee, as well as Chair of the Senate Appropriations Committee. Before his work as an elected official, he was Senior Director at IHS Markit, where he led a global portfolio of energy products, events, and partnerships. Dr. Hansen holds a BSc in Nuclear Engineering from Kansas State University; a Graduate Diploma of Civil Engineering from the University of the Witwatersrand, South Africa; a Master of Science in Engineering Systems from MIT; and a Ph.D. in Economic Geography from Oxford University.  In addition to his current role in the state senate, Hansen serves as Chairman of the Board of Western Freedom, a non-profit dedicated to integrating the power system and RTO in the West.  He is also the Co-Founder and Director at the Colorado Energy & Water Institute and as Founder of the Colorado Science and Engineering Policy Fellowship.
-
-Sachin Mittal is stewarding KERNEL at Gitcoin.
-
-## Moderator
-
-Jake Interrante is Editor in Chief of the Chicago Policy Review and an MPP candidate ’21 at the University of Chicago Harris School of Public Policy. Previously, Jake worked as a policy professional in the community development finance field. During his time working with Massachusetts Housing Partnership’s ONE Mortgage Program, he helped working-class families buy homes in otherwise unaffordable parts of the State. Before that, he helped finance amenities in economically disadvantaged communities as Development Coordinator for Partners for the Common Good, a DC-based CDFI Loan Fund. His published work on comparative borrower outcomes in the ONE Mortgage program and FHA Mortgage Program appeared in the March 2020 edition of Cityscape. Jake also holds a B.A.s in Public Policy and Political Science from the University of Chicago.`,
-    slidoUrl: "https://app.sli.do/event/tortvll0",
+See RxC President Matt Prewitt (who will lead the event) talk about the exciting election outcomes with CEO Jennifer Lyn Morone and Project Developer Alex Randaccio. From delegation to deliberation to the curation of the ballot and then the election, many lessons were gleaned and we're looking forward to sharing how we can make RxC Voice better.`,
+    slidoUrl: "https://app.sli.do/event/5mxfke4o",
   },
 };
