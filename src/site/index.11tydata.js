e6edf4b7889a8a84679e22eb5ec473f018a39d77
alexrandaccio <alex@radicalxchange.org>
2021-12-03 00:18:38 -0500

    before rxc tv

--- src/site/index.11tydata.js
+++ src/site/index.11tydata.js
@@ -1,37 +1,15 @@
 module.exports = {
   rxcTv: {
     isOn: false,
-    title: "Exploring Social Technologies for Democracy",
-    description: `September 30th, 4:00 pm to 5:30 pm UTC
+    title: "RxC TV",
+    description: `December 3rd, 11:00 am to 2:00 pm UTC
 
-We see democracy as ideally a process of co-creating the conditions of our shared lives, solving our collective problems, and learning about life from and with each other. The less force and the more creativity, aliveness, and intelligence we can bring to this process, the better. This requires a lot of communication and conversation to share information, ideas, experiences, needs, values, energy and so much more - in ways that actually work.
+Welcome to the 3rd Annual RadicalxChange unConference
 
-Many different approaches to doing this sharing and talking have been created. When such ways are formalized as methods that can be applied and taught, we call them “technologies”. Most of the social technologies for democracy we work with are grounded in conversation - discussion, dialogue, deliberation, choice-creating, negotiation, collective visioning, and various forms of council, assembly, conference, and so on. There are different dynamics and nuances among them, and there are experts trained to facilitate the theory and/or practice.
+In addition to the in-person and online events, RadicalxChange is putting together RxC TV, a unique and never before seen 3-part program that will be streamed LIVE the day before each Open Space unConference and thematically related to the succeeding event, to inspire and learn.
 
-Democratic technologies, thought of and used in this way, can be applied in creative new ways that enable people to become more engaged with each other, with better results and less wasted, counter-productive energy, thus moving towards more successful, enjoyable self-governance. They operate at a variety of scales from small groups, organizations, and networks to whole societies.
-
-Please join us for this conversation exploring social technologies and their role in fostering democracy with Kaliya Young, Heidi Saul, and Tom Atlee, moderated by RxC CEO Jennifer Morone.
-
-## Speakers
-
-Kaliya Young (@identitywoman) is the founder of Unconference.net, an event design and facilitation for professional communities. Her facilitation career began with the co-founding of the Internet Identity Workshop in 2005. It still meets twice a year and is the world’s leading interactive, industry shaping event that brings together the largest concentration of talent dedicated to designing and building online identity systems that empower individuals.
-
-Over the years founded, designed and/or facilitated over 100 Unconferences. Events she founded, or co-founded include, She’s Geeky, The UnMoney Convergence, Digital Death Day, and Open Government Playbook. Her clients have included Digital Enlightenment Forum, Mashup Camp, The Mass Technology Leadership Council Innovation, Ruby on Rails Camp, Scala Lift Off, IEEE - Engineering Medicine Biology, Read Write Web, Creative Unconference, Recent Changes Camp.
-
-She has training in several different methodologies including Graphic Recording and Facilitation, Innovation Games, Open Space Technology, Polarity Management, and her firm is a member of National Coalition for Dialogue and Deliberation.
-
-She is also an expert and leader in the fields of user-centric digital identity, self-sovereign identity and personal data and has appeared in this capacity in three different RxC events. She blogs, and tweets under her community handle – “Identity Woman“.
-
-Heidi Nobantu Saul, MAM (@nobantu) is an international facilitator and process design professional. She specializes in planning and facilitating large and small Open Space Technology (OST) meetings for organizations, agencies, educational institutions, retreats, strategic planning and open events such as unConferences. Much of her facilitation practice involves working with people who are working on the leading edge of change. She has co-produced and facilitated several annual Open Space unConferences such as: the University API Winter Workshop sponsored by BYU (6 years) and, for the past 13 years, the Internet Identity Workshop (#IIW), the world’s leading interactive, industry shaping event that brings together the largest concentration of talent dedicated to designing and building online identity systems that empower individuals. Her OST clients range from Sony PlayStation, Forum One, Microsoft, Phoenix Zones Initiative and IEEE Standards Association to educational institutions, BYU, NYU, University of Western Australia and the California Institute of Integral Studies. And locally in Santa Fe, Assistance Dogs of the West a leader in training Court House Dogs and FBI crisis response canines. Since the beginning of the Covid Pandemic in early 2020 Heidi has co-produced and facilitated over 15 small/large, 1 to 3 Day) Open Space online meetings/events in QiqoChat.
-
-Along with facilitation she enjoys working with others co-developing events, training courses and tools (process design) for self-exploration, expanding self and collective awareness and working sustainably together.
-
-Tom Atlee (@tomatlee) is co-founder of the nonprofit Co-Intelligence Institute in Eugene, Oregon. His book titles give a good sense of his life’s work - The Tao of Democracy, Empowering Public Wisdom, Participatory Sustainability, and Reflections on Evolutionary Activism. His beautifully illustrated Wise Democracy Pattern Language offers an encyclopedia of democratic innovations and of approaches to generating collective wisdom. It even comes with a card set to explore ideas, challenges and possibilities through the wise democracy lens. His blog - Random Communications from an Evolutionary Edge - explores an even wider range of topics in provocative ways. (He really likes words like “explore” and “possibility".) He writes, philosophizes, networks, and Zooms into fascinating conversations from his room in an urban coop with 8 housemates and from the rural dome home of his sweetheart and her loving pit bull. Tom is a true public intellectual having spent 32 years researching and writing about co-intelligence with no formal institutional support beyond his small nonprofit. He is supported by the people on his mailing list which you can join by emailing him at cii@igc.org (subject: SUBSCRIBE).
-
-## Moderator
-
-Jennifer Morone (@jlmorone) is an artist and CEO of RadicalxChange Foundation. As an artist, she makes work that seeks to disrupt the narratives that humans create – such as corporations and nation states, and has been dealing with the problematic of the position of the individual in the contemporary world of work for many years. In 2014 she registered herself as a corporation – Jennifer Lyn Morone™ Inc, of which she is the director, the shareholder, and the basic product, in order to try to provoke the ruling economic and legal system from within. Since 2019 she has been working on The Scheme of Things, a co-creative process that brings to life imaginations of what a desirable post-work world might look like through various media.`,
-    slidoUrl: "https://app.sli.do/event/hmftj1pa",
+Broadcasts will be on December 3rd, 9th, and 16th corresponding to their respective time zones.`,
+    slidoUrl: "",
   },
   popup: {
     isOn: true,
