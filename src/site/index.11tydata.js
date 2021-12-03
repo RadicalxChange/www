17f22384d3d118c343688fd21a4bf5f18a1f0be0
alexrandaccio <alex@radicalxchange.org>
2021-12-03 00:39:32 -0500

    edit copy

--- src/site/index.11tydata.js
+++ src/site/index.11tydata.js
@@ -1,14 +1,32 @@
 module.exports = {
   rxcTv: {
     isOn: false,
-    title: "RxC TV",
-    description: `December 3rd, 11:00 am to 2:00 pm UTC
+    title: "3rd Annual RadicalxChange unConference Opening Livestream",
+    description: `December 3rd, 11:00 am to 3:00 pm UTC
 
 Welcome to the 3rd Annual RadicalxChange unConference
 
+Opening Livestream Schedule:
+
+RxC TV
+11:00 am to 2:00 pm UTC
+
 In addition to the in-person and online events, RadicalxChange is putting together RxC TV, a unique and never before seen 3-part program that will be streamed LIVE the day before each Open Space unConference and thematically related to the succeeding event, to inspire and learn.
 
-Broadcasts will be on December 3rd, 9th, and 16th corresponding to their respective time zones.`,
+Broadcasts will be on December 3rd, 9th, and 16th corresponding to their respective time zones.
+
+Opening Ceremony
+2:30 pm to 3:00 pm UTC
+
+A panel from the RadicalxChange Foundation welcomes you to the 3rd Annual RadicalxChange unConference.
+
+## Speakers
+
+Jennifer Morone (@jlmorone) is an artist and CEO of RadicalxChange Foundation. As an artist, she makes work that seeks to disrupt the narratives that humans create – such as corporations and nation states, and has been dealing with the problematic of the position of the individual in the contemporary world of work for many years. In 2014 she registered herself as a corporation – Jennifer Lyn Morone™ Inc, of which she is the director, the shareholder, and the basic product, in order to try to provoke the ruling economic and legal system from within. Since 2019 she has been working on The Scheme of Things, a co-creative process that brings to life imaginations of what a desirable post-work world might look like through various media.
+
+Matthew Prewitt is RadicalxChange Foundation’s president, a writer and blockchain industry advisor, and a former plaintiff’s side antitrust and consumer class action litigator and federal law clerk.
+
+Paula Berman is a member of the RadicalxChange Foundation core team. Prior to that, she was a member of the RadicalxChange Fellowship 2021 Cohort. Paula has a wealth of experience studying the impact of blockchain technology on local and global governance, as well as implementing participatory technologies and researching decentralized identity protocols.`,
     slidoUrl: "",
   },
   popup: {
