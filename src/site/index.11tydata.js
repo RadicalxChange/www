af5bbb6426a3f5a09886fe5e44ad82aaf6c232fb
alexrandaccio <alex@radicalxchange.org>
2021-12-22 12:30:12 -0500

    remove open invitation

--- src/site/index.11tydata.js
+++ src/site/index.11tydata.js
@@ -16,9 +16,7 @@ Broadcasts will be on December 3rd, 9th, and 16th corresponding to their respect
     title: "Announcing the Next Annual RxC Conference December 2021",
     bodyText: `Democracy is in crisis at a critical moment when we face seemingly insurmountable challenges from growing economic disparity, climate change, and a global pandemic. Despite increased and active involvement of citizens in the democratic process, our governing systems, stifled with deepening polarization, are halting progress.
 
-Fortunately, pockets of innovators working inside and outside of states are developing new tools and methods for making group decisions, governing shared resources, and building tomorrow’s democratic infrastructure. This culture of “civic technology”, partly inspired by the activists of g0v in Taiwan and drawing energy from many communities and disciplines, is at the heart of the RadicalxChange movement.
-
-Whether you identify as forward-thinking technologists, social scientists, artists, activists, community leaders, entrepreneurs, or everyday people dedicated to this shared mission, please join us for the 3rd Annual RadicalxChange unConference to connect with others and co-create a radically egalitarian and participatory future.`,
+Fortunately, pockets of innovators working inside and outside of states are developing new tools and methods for making group decisions, governing shared resources, and building tomorrow’s democratic infrastructure. This culture of “civic technology”, partly inspired by the activists of g0v in Taiwan and drawing energy from many communities and disciplines, is at the heart of the RadicalxChange movement.`,
     link: "/2021-conference",
   },
 };
