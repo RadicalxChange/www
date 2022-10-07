d6aad7f3c3bfb4cf24ff5d8fe3ccbe9fc88d5b0e
alexrandaccio <alex@radicalxchange.org>
2022-10-07 18:16:35 -0400

    more edits

--- src/site/index.11tydata.js
+++ src/site/index.11tydata.js
@@ -28,11 +28,13 @@ Matt Prewitt (@m_t_prewitt) is RadicalxChange Foundation’s president, a writer
     title: "Announcing the RxC Warsaw Conference & Freedom Games",
     bodyText: `With Europe at war, the world has profoundly changed since our conferences in Taipei and Denver last December. More than ever, we must step up together and embrace the power of diverse cooperation.
 
-This October, the RxC community will safely gather in Łódź, Poland for the [2022 Freedom Games (Oct 14-16)](https://igrzyskawolnosci.pl/en/), and subsequently in neighboring Warsaw, Poland, for a day of solidarity, inspiration, and co-creation.
+This October, the RxC community will gather in-person (& Online) in Łódź, Poland (Oct 15-16) and in Warsaw, Poland (Oct 18).
 
-Together in Poland, we will learn of pioneering digital democracy practices in Ukraine, Estonia, and across Eastern Europe. We will experience works of art as windows to connection, and examine European institutions through the frame of pluralistic governance. We will uplift the work of activists, social fabric weavers, and critical voices. We will consider European cases for community currencies, data cooperatives, decentralized identity systems, communication tools, and other pro-social digital infrastructures and civic technologies.
+At the [2022 Freedom Games](https://igrzyskawolnosci.pl/en/) in Łódź, with the support of [Fundacja Liberté!](https://liberte.pl/), RadicalxChange has an entire program of panels and keynotes on “[21st Century Infrastructure: From State Capacity to Shared Capacity](/events/2022-freedom-games/).”
 
-We invite you to join us at the [2022 Freedom Games in Łódź, Poland, & Online (Oct 14-16)](https://igrzyskawolnosci.pl/en/), where RadicalxChange is a partner, and at our conference “RadicalxChange Warsaw: Europe and The Futures of Pluralism,” which will be staged with the support of [Wildland](https://wildland.io). The Warsaw event will take place on Tuesday, the 18th of October 2022, at the [Mała Warszawa](https://goo.gl/maps/J3r6PqmAJTiAcDmZ6) conference center.`,
+And in Warsaw, with the support of Wildland, RadicalxChange is hosting a community event called “[RxC Warsaw: Europe and The Futures of Pluralism](/events/2022-warsaw/).”
+
+We invite you to Poland, where together we will learn of pioneering digital democracy practices in Ukraine, Estonia, and across Eastern Europe. We will experience works of art as windows to connection, and examine European institutions through the frame of pluralistic governance. We will uplift the work of activists, social fabric weavers, and critical voices. And we will consider European cases for community currencies, data cooperatives, decentralized identity systems, communication tools, and other pro-social digital infrastructures and civic technologies.`,
     link: "/events/2022-warsaw",
   },
 };
