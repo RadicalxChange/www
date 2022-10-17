9ed349c1d84e0c699669f364147df935c63ac502
alexrandaccio <alex@radicalxchange.org>
2022-10-17 23:08:14 +0200

    before tv

--- src/site/index.11tydata.js
+++ src/site/index.11tydata.js
@@ -2,26 +2,16 @@ module.exports = {
   rxcTv: {
     isOn: false,
     title: "Governance and Technological Progress",
-    description: `May 13th, 3:30 pm to 5:00 pm UTC
+    description: `October 18th, 9:00am to 6:00 pm CEST
 
-The narrative of technological progress has arguably overlooked progress in governance. Why is that? What are the risks of exponential technology? Why is governance important and can it get better, too? Must it always be catching up to technology? What might democracy at scale look like?
+With Europe at war, the world has profoundly changed since our conferences in Taipei and Denver last December. More than ever, we must step up together and embrace the power of diverse cooperation.
 
-Please join us for this conversation examining technological progress in governance and whether its narrative has truly been overlooked with Deepti Doshi, Nathan Schneider, and Divya Siddarth, moderated by RxC President Matt Prewitt.
+This October, the RxC community will gather in-person (& Online) in Łódź, Poland (Oct 15-16) and in Warsaw, Poland (Oct 18).
 
-Register to join this thought-provoking panel and get involved in the conversation by asking questions on Slido, tweeting us @radxchange (#RxCLiveTalk), and sharing this event.
+Here we invite you to our conference in Warsaw: “RadicalxChange Warsaw: Europe and The Futures of Pluralism.” The event will take place on Tuesday, October 18th, 2022 at the Mała Warszawa conference center, with the support of [Wildland](https://wildland.io).
 
-SPEAKERS:
-
-Deepti Doshi (@deeptidoshi) is a community organizer who has been working at the intersection of social change, social media, and leadership development across the private, non-profit and public sectors. As a Director at Meta, Deepti helped establish the New Product Experimentation team and created the Community Partnerships team. She founded Haiyya, India’s largest community organizing platform; Escuela Nueva India, an education company that serves the urban poor; the Fellows Program at Acumen Fund to build leaders for the social enterprise sector. She holds degrees in public policy from Harvard Kennedy School and in business from Wharton Business School, and in psychology from the University of Pennsylvania. She is the Co-Director of New_ Public.
-
-Nathan Schneider (@ntnsndr) is an assistant professor of media studies at the University of Colorado Boulder, where he leads the Media Enterprise Design Lab and the MA program in Media and Public Engagement. He is the author of Everything for Everyone: The Radical Tradition that Is Shaping the Next Economy, published by Nation Books, and two previous books, God in Proof: The Story of a Search from the Ancients to the Internet and Thank You, Anarchy: Notes from the Occupy Apocalypse, both published by University of California Press. Recent scholarship has been published in New Media & Society, Feminist Media Studies, the Georgetown Law Technology Review, and Media, Culture & Society, among other journals. He has also reported for publications including Harper’s, The Nation, The New Republic, The Chronicle of Higher Education, The New York Times, The New Yorker, and others, along with regular columns for America, a national Catholic magazine. He has lectured at universities including Columbia, Fordham, Harvard, MIT, NYU, the University of Bologna, and Yale. In 2015, he co-organized “Platform Cooperativism,” a pioneering conference on democratic online platforms at The New School, and co-edited the subsequent book, Ours to Hack and to Own: The Rise of Platform Cooperativism, a New Vision for the Future of Work and a Fairer Internet. Follow his work on social media at @ntnsndr or at his website, nathanschneider.info.
-
-Divya Siddarth (@divyasiddarth) is a political economist and social technologist. Her current research focus is around promoting and preserving the digital commons, building the technology and policy infrastructure for decentralized governance, and devising frameworks for collectively-focused, rather than centrally concentrated, AI. She builds technology at Microsoft, imagines new technological worlds with Verses, and thinks about how to govern it all with RadicalxChange, Metagov, and the Ostrom Workshop.
-
-MODERATOR:
-
-Matt Prewitt (@m_t_prewitt) is RadicalxChange Foundation’s president, a writer, and blockchain industry advisor, and a former plaintiff's side antitrust and consumer class action litigator and federal law clerk.`,
-    slidoUrl: "https://app.sli.do/event/tjW7711jARfGXbgJueJZb1",
+Together we will learn of pioneering digital democracy practices in Ukraine, Estonia, and across Eastern Europe. We will experience works of art as windows to connection, and examine European institutions through the frame of pluralistic governance. We will uplift the work of activists, social fabric weavers, and critical voices. And we will consider European cases for community currencies, data cooperatives, decentralized identity systems, communication tools, and other pro-social digital infrastructures and civic technologies.`,
+    slidoUrl: "https://app.sli.do/event/hMbwwnMR3XfHqd541maCk3",
   },
   popup: {
     isOn: true,
