40de2b5a2eead183e9f29f773b9e42abd1d57dcd
Alex Randaccio <acrandaccio@gmail.com>
2024-05-15 16:09:44 -0400

    init book launch popup & announcement

--- src/site/index.11tydata.js
+++ src/site/index.11tydata.js
@@ -10,13 +10,17 @@ Join RxC and Serpentine UK in a discussion with the authors of the post: Victori
     slidoUrl: "",
   },
   popup: {
-    isOn: false,
-    title: "Beyond Cultures of Ownership: Emerging Strategies for Interdependence",
-    bodyText: `**RadicalxChange & Serpentine Arts Technologies with Dark Matter Labs & Somerset House co-host a one-day gathering in London on 3 Nov 2023.**
+    isOn: true,
+    title: "Plurality Book Launch",
+    bodyText: `**Out now: *Plurality: The Future of Collaborative Technology and Democracy*, by E. Glen Weyl, Audrey Tang, and Community**
 
-Join us on the 3rd of November, 2023, at Somerset House, London, to explore and strategise how art and culture can play an active part in reconfiguring ownership. We see ‘ownership’ as the underlying power dynamic of our time that can either create or block possibilities for rebalancing our relationships with technology, the planet, and one another.
+    Digital technology has catalyzed polarization, inequality, loneliness and fear.
 
-We invite artists, activists, researchers, technologists, and policymakers active and interested in connecting the dots between cultural production, political economies, and systems change. By creating a space for all of us to come together from different contexts, we hope to sketch out connections between new imaginaries and practises that instigate more plural and relational protocols for interdependence.`,
-    link: "/events/2023-london",
+    Plurality details how Digital Minister Audrey Tang and her collaborators – architects of Taiwan’s internationally acclaimed digital democracy – achieved inclusive, technology-fueled growth that harnesses digital tools to strengthen both social unity and diversity.
+    
+    From intimate digitally empowered telepathy to global trade running on social networks rather than money, Plurality offers tools to radically enrich relationships while making sure we leave no one behind.
+    
+    The ideas promise to transform every sector from health care to media, as illustrated by the way it has been written: as a chorus of open, self-governing collaboration of voices from around the globe.`,
+    link: "/media/announcements/plurality-book-launch/",
   },
 };
---
+++ src/site/media/announcements/2024-05-20_plurality-book-launch.md
@@ -0,0 +1,25 @@
+---
+layout: "layouts/announcement.njk"
+date: "2024-05-20"
+title: "Plurality Book Launch"
+postHeader: "Plurality: The Future of Collaborative Technology and Democracy"
+---
+
+![Plurality Book Cover](/images/announcements/plurality-book-cover.png)
+
+
+Digital technology has catalyzed polarization, inequality, loneliness and fear.
+
+Plurality details how Digital Minister Audrey Tang and her collaborators – architects of Taiwan’s internationally acclaimed digital democracy – achieved inclusive, technology-fueled growth that harnesses digital tools to strengthen both social unity and diversity.
+
+From intimate digitally empowered telepathy to global trade running on social networks rather than money, Plurality offers tools to radically enrich relationships while making sure we leave no one behind.
+
+The ideas promise to transform every sector from health care to media, as illustrated by the way it has been written: as a chorus of open, self-governing collaboration of voices from around the globe.
+
+[Buy the book now](/)
+
+## Insert title of film
+
+Insert a blurb about the film here.
+
+[EMBEDDED VIDEO]
